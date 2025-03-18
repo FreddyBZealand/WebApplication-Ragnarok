3b4d27a931ebf7c1c35f3551fbb28a8978d1600b
FreddyBZealand <frb009@edu.zealand.dk>
2025-03-18 11:35:47 +0100

    .

---
+++ WebApplication Ragnarok/Pages/Shared/Søren Berlev.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication_Ragnarok.Pages.Shared
+{
+    public class Søren_BerlevModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
