2e6e07976c157114d6699e79ca81d8ee3a32091b
Emre Can Caliskan <emc004@edu.zealand.dk>
2025-03-21 10:25:37 +0100

    --

---
+++ WebApplication Ragnarok/Pages/Shared/qr.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication_Ragnarok.Pages.Shared
+{
+    public class qrModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
